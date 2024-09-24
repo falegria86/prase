be76ba57425f816e70649da752716000d8d236a8
fgalegria <francisco.gomez@comsys.mx>
2024-09-24 09:51:15 -0700

    Fix: typeerrors

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,9 +1,6 @@
 import Inicio from "@/components/Inicio";
-import { currentUser } from "@/lib/auth";
 
 export default async function Home() {
-  const user = await currentUser();
-
   return (
     <>
       <Inicio />
